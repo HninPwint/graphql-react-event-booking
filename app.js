e3b17fd3706887cd4f40be73f21b1577e54b9681
Hnin Pwint Tin <tin.hnin@sinonet.sg>
2019-06-27 12:15:02 +0800

    First GraphQl query and mutation with mongodb Atalas

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/Event');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }  
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }  
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc, _id: event.id };
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                });
+        },
+        createEvent: args => {
+            console.log('Event', Event);
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date)
+            });
+            return event.save()
+             .then(result => {
+                 console.log(result);
+                 return { ...result._doc, _id: result._doc._id.toString() };
+             })
+              .catch( err => {
+                  console.log(err);
+                  throw err;
+              });
+        }
+    },
+    graphiql: true,
+})
+);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-62tbx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(
+    () => {
+        app.listen(3000);
+    })
+    .catch(err => {
+    console.log(err);
+});
+
+
+
+/*
+mutation {
+  createEvent(eventInput: {
+    description: "Study React",
+    price: 34.44
+    title: "Technical"
+    date: "2019-06-27T03:39:48.820Z"
+  }) {
+    _id
+    description
+  }
+}
+*/
