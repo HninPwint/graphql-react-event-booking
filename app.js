90ab65c4bcbb2c8bb65045284e35131e71a3cc16
Hnin Pwint Tin <tin.hnin@sinonet.sg>
2019-06-28 15:06:18 +0800

    Install Prettier and Format app.js

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphQlHttp = require('express-graphql');
-const mongoose = require('mongoose');
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolver = require('./graphql/resolver/index');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphQlHttp = require("express-graphql");
+const mongoose = require("mongoose");
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolver = require("./graphql/resolver/index");
 
 const app = express();
 app.use(bodyParser.json());
 
+app.use(
+  "/graphql",
+  graphQlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolver,
+    graphiql: true
+  })
+);
 
-app.use('/graphql', graphQlHttp({
-        schema: graphQlSchema,
-        rootValue: graphQlResolver,
-        graphiql: true,
-}
-));
-
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-62tbx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(
-    () => {
-        app.listen(3000);
-    })
-    .catch(err => {
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-62tbx.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
     console.log(err);
-});
-
-
+  });
 
 /*
 mutation {
