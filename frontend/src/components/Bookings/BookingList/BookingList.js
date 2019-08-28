16c23c114cab585f9822b30c296ad8d50b7fa48b
Hnin Pwint Tin <tin.hnin@sinonet.sg>
2019-08-28 17:59:51 +0800

    Create Booking Page

---
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./BookingList.css";
+
+
+
+const bookingList = props => {
+  const bookings = props.bookings.map(booking => {
+    return (
+      <li className="bookings__item" key={booking._id}>
+        <div className="bookings__item-data">
+          {booking.event.title} - {""}{" "}
+          {new Date(booking.createdAt).toLocaleString()}
+        </div>
+        <div className="bookings__item-actions">
+          <button className="btn" onClick={props.onDelete.bind(this,booking._id)}>Cancel</button>
+        </div>
+      </li>
+    );
+  });
+  return <ul className="bookings__list">{bookings}</ul>;
+};
+
+export default bookingList;
